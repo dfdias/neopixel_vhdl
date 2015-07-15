3ea74c4b2d005672faa617c92773554c9aec0205
Duarte Dias <dfdias@ua.pt>
2015-07-15 09:17:30 +0100

    started toporgram the behavioral of the project(state machine sosf)

--- NeoPixel.vhd
+++ NeoPixel.vhd
@@ -5,7 +5,22 @@ use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 entity NeoPixel is
 generic port(N : 1) : number of neopixel in  string
-port (clk : in std_logic;
-	  rst : in std_lolgic;
+port (clk : in std_logic;			-- i don't know the frequency, yet but i'll use a crystal @50MHz
+	  rst : in udo std_lolgic;			    -- reset button of the system
 	  SW  : in std_logic_vector(2 downto 0);--for demo and test purposes we are going to use those to change color;
-	  OUT : out is array of std_logic_vector(7 downto 0) range 0 to N-1);
+	  data_out : out std logic;--std_logic_vector(7 downto 0)); --to be conecte on datapin
+
+end NeoPixel;
+
+architecture Behav of NeoPixel is
+
+signal s_time, s_time_next;
+signal s_color,s_color_next: std_logic;--std_logic_vector(7 downto 0);
+subtype state (E0,E1,E2,E3);
+signal PS,NS : state;
+
+state_proc : process (clk, rst)
+begin
+ if rising_edge(clk) then
+ 	s_time_next <= s_time;
+ end if ;
