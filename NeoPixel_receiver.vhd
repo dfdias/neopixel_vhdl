909ca66c4d4738e7897ef1f2d1aa8a7df6d8b5cb
Duarte Dias <dfdias@ua.pt>
2015-07-17 10:40:29 +0100

    as

--- NeoPixel_receiver.vhd
+++ NeoPixel_receiver.vhd
@@ -5,7 +5,8 @@ use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
 entity NeoPixel_receiver is --this module will receive some data from serial port
-port()
+port(clk :in std_logic;
+	 )
 end NeoPixel_receiver;
 
 architecture Behav of NeoPixel_receiver is
