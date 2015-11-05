fe13d9f6d65b5a99e48ceda0167b1977d5082c18
Duarte Dias <dfdias@ua.pt>
2015-11-05 11:24:38 +0000

    some more syntax errors solved...

--- NeoPixel_controller.vhd
+++ NeoPixel_controller.vhd
@@ -1,13 +1,14 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
+
 entity NeoPixel_controller is
---generic port (N := 1);                 -- number of neopixel in string*/
+generic (N : integer := 1);                 -- number of neopixel in string*/
 port (clk : in std_logic;			-- i don't know the frequency yet, but i'll use a crystal @50MHz
-	    rst : in  std_logic;		-- reset button of the system will be moved to a shell file
-	    SW  : in std_logic_vector(2 downto 0); --for demo and test purposes we are going to use those to change color;
+	   rst : in  std_logic;		-- reset button of the system will be moved to a shell file
+	   sw  : in std_logic_vector(2 downto 0); --for demo and test purposes we are going to use those to change color;
 	  --RGB : in std_logi_vector(23 downto 0);  -- color datain from serial input(color wheel routine)
-	    refresh : out std_logic;
+	   refresh : out std_logic;
       data_out : out std_logic_vector(23 downto 0)); --to be conected on datapin
 
 end NeoPixel_controller;
@@ -17,7 +18,7 @@ architecture Behav of NeoPixel_controller is
 constant reset_time: integer := 25; --cascade transmition time in us
 signal s_time, s_time_next : integer range 0 to 1000;
 signal s_i, s_i_next : integer range 0 to N;
-signal s_color,s_color_next: std_logic;--std_logic_vector(23 downto 0);
+signal s_color,s_color_next: std_logic_vector(23 downto 0);
 --signal d is array of std_logic_vector(23 downto 0) range 1 to N; --array with the size of an N number of pixels on String
 --signal PS,NS : state;
 --S0,S1,S2;
@@ -27,23 +28,24 @@ begin
 
 
 s_R <= "11111111";
-s_G <= others('0');
+s_G <= (others => '0');
 s_B <= "11111111";
 
 
-state_proc : process (clk, rst) --state machine sync manager process
+state_proc : process (clk,rst) --state machine sync manager process
 begin
  if rising_edge(clk) then
    s_time_next <= s_time + 1;
- -- PS <= NS;
+--	PS <= NS;
   if rst <= '1' then
-   s_time <= '0';
+   s_time <= 0;
   end if ;
  end if ;
 end process;
 
 
 comb_proc: process(s_color,s_color_next,s_i,s_i_next) 
+begin
  refresh <= '0';
  if s_i < N then
  	s_color_next <= s_G & S_R & S_B;
--- NeoPixel_mapping.vhd
+++ NeoPixel_mapping.vhd
@@ -3,8 +3,8 @@ use IEEE.STD_LOGIC_1164.all;
 use IEEE.NUMERIC_STD.all;
 
 entity NeoPixel_mapping is
---generic (K := num_pixels);								--replace num_pixelf by the number of pixel connectedon the string 
-port(clk50		:  in std_logic;
+generic (K : integer := 1);								--replace num_pixelf by the number of pixel connectedon the string 
+port(clk_50		:  in std_logic;
 	 sw  			:  in std_logic_vector(3 downto 0);
 	 pin_out 	: out std_logic);
 end NeoPixel_mapping;
@@ -16,17 +16,18 @@ signal s_refresh : std_logic;
 
 begin
 
-NeoPixel_controller: entity work.NeoPixel_controller(Behav)
-	--generic port(N => K);
+controller: entity work.NeoPixel_controller(Behav)
+	generic map(N => 1);
 	port map(
-				clk => clk50,
-				rst => sw(0),
-				sw  => sw(3 downto 1),
-				refresh => s_refresh,
-				data_out =>s_raw_out);
+			 clk => clk_50,
+			 rst => sw(0),
+			 sw  => sw(3 downto 1),
+			 refresh => s_refresh,
+			 data_out =>s_raw_out);
 
 pixel_decoder: entity work.output_manager(Behav)
-	port map(clk => clk50,
+	port map(
+		     clk => clk_50,
 			 refresh =>s_refresh,
 			 input =>s_raw_out,
 			 rst => '0',
